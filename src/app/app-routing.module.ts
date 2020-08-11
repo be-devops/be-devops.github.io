60bd372596c71e9b485e90e5301892f75411c719
ngthuc <lenguyenthuc.edu@gmail.com>
2020-08-11 15:52:06 +0700

    Add new template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
-import {AuthGuard} from './guards/auth.guard';
 import {ServerErrorComponent} from './errors/server-error/server-error.component';
 import {NotFoundComponent} from './errors/not-found/not-found.component';
 import {ForbidenComponent} from './errors/forbiden/forbiden.component';
-import {HomePageComponent} from "./pages/home-page/home-page.component";
-import {AboutPageComponent} from "./pages/about-page/about-page.component";
-import {CoursePageComponent} from "./pages/course-page/course-page.component";
-import {StaffPageComponent} from "./pages/staff-page/staff-page.component";
-import {BlogPageComponent} from "./pages/blog-page/blog-page.component";
-import {ContactPageComponent} from "./pages/contact-page/contact-page.component";
-import {SearchResultPageComponent} from "./pages/search-result-page/search-result-page.component";
+import {HomeComponent} from "./pages/home/home.component";
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'login', component: LoginComponent, pathMatch: 'full'},
-  {path: 'home', component: HomePageComponent},
-  {path: 'about', component: AboutPageComponent},
-  {path: 'courses', component: CoursePageComponent},
-  {path: 'teacher', component: StaffPageComponent},
-  {path: 'blog', component: BlogPageComponent},
-  {path: 'contact', component: ContactPageComponent},
-  {path: 'search-result', component: SearchResultPageComponent},
   {path: '500', component: ServerErrorComponent},
   {path: '403', component: ForbidenComponent},
   {path: '**', component: NotFoundComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,13 @@ import {MenuComponent} from './layout/menu/menu.component';
 import {FooterComponent} from './layout/footer/footer.component';
 import {HeaderComponent} from './layout/header/header.component';
 import {LoginComponent} from './login/login.component';
-import {JwtModule} from '@auth0/angular-jwt';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {TokenInterceptor} from './auth/token.interceptor';
 import {DatePipe} from '@angular/common';
 import {NotFoundComponent} from './errors/not-found/not-found.component';
 import {ServerErrorComponent} from './errors/server-error/server-error.component';
 import {ForbidenComponent} from './errors/forbiden/forbiden.component';
-import {HomePageComponent} from "./pages/home-page/home-page.component";
-import {AboutPageComponent} from "./pages/about-page/about-page.component";
-import {CoursePageComponent} from "./pages/course-page/course-page.component";
-import {StaffPageComponent} from "./pages/staff-page/staff-page.component";
-import {BlogPageComponent} from "./pages/blog-page/blog-page.component";
-import {ContactPageComponent} from "./pages/contact-page/contact-page.component";
-import {SearchResultPageComponent} from "./pages/search-result-page/search-result-page.component";
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -32,13 +25,7 @@ import {SearchResultPageComponent} from "./pages/search-result-page/search-resul
     NotFoundComponent,
     ServerErrorComponent,
     ForbidenComponent,
-    HomePageComponent,
-    AboutPageComponent,
-    CoursePageComponent,
-    StaffPageComponent,
-    BlogPageComponent,
-    ContactPageComponent,
-    SearchResultPageComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
